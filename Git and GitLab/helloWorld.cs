390ecbaf8501d45402f2a786620ba202b5011137
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-08-02 13:02:17 +0530

    Initial commit

---
+++ Git and GitLab/helloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world !");
+            Console.ReadKey();
+        }
+    }
+}
